0029c06e081ae78ecd93e0f7cd88a621ec7aec43
Bohdan Dhzus <b.dhzus@yahoo.com>
2018-12-08 19:33:05 +0200

    render mock data

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 new Vue({
     el: '#app',
     data: {
-        
+        todos: [
+            {   
+                done: false,
+                text: 'do smth'
+            },
+            {   
+                done: false,
+                text: 'do smth'
+            },
+            {   
+                done: true,
+                text: 'done'
+            }
+        ]    
     }
 })
